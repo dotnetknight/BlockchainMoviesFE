d621a6e90996bcca565ca0c8c609f8fdca07be9e
Niko Kantaria <nika.qantaria98@gmail.com>
2022-03-02 23:26:41 +0400

    Added structured adding for movides, id generation

--- contracts/Movies.sol
+++ contracts/Movies.sol
@@ -7,16 +7,58 @@ contract Movies {
         uint256 Id;
         string Name;
         string Director;
+        ActorsStruct[] Actors;
     }
 
-    MoviesStructure[] MoviesArray;
+    struct ActorsStruct {
+        uint256 Id;
+        string Name;
+    }
 
-    function AddMovie(
-        uint256 _Id,
-        string memory _Name,
-        string memory _Director
-    ) public {
-        MoviesArray.push(MoviesStructure(_Id, _Name, _Director));
+    struct MovieIdStruct {
+        uint256 movieId;
+    }
+
+    struct ActorIdStruct {
+        uint256 actorId;
+    }
+
+    MovieIdStruct movieCounter;
+    ActorIdStruct actorCounter;
+    MoviesStructure[] MoviesArray;
+    mapping(uint256 => MoviesStructure) public mapMovie;
+
+    function NextMovieId(MovieIdStruct storage _counter)
+        internal
+        returns (uint256)
+    {
+        _counter.movieId = _counter.movieId + 1;
+        return _counter.movieId;
+    }
+
+    function NextActorId(ActorIdStruct storage _counter)
+        internal
+        returns (uint256)
+    {
+        _counter.actorId = _counter.actorId + 1;
+        return _counter.actorId;
+    }
+
+    function AddMovie(MoviesStructure memory _movie) public {
+        uint256 _movieId = NextMovieId(movieCounter);
+
+        MoviesStructure storage movie = mapMovie[_movieId];
+        movie.Id = _movieId;
+        movie.Director = _movie.Director;
+        movie.Name = _movie.Name;
+
+        for (uint256 i = 0; i < _movie.Actors.length; i++) {
+            movie.Actors.push(
+                ActorsStruct(NextActorId(actorCounter), _movie.Actors[i].Name)
+            );
+        }
+
+        MoviesArray.push(movie);
     }
 
     function GetMovies() public view returns (MoviesStructure[] memory) {
